14dd6f3b3635e90f6e39c47d76e7112210f638d8
AliceMonte <lol.gerda96@gmail.com>
2023-01-07 14:45:49 +0300

    lesson 71-76

--- my_first/my_site/urls.py
+++ my_first/my_site/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 path('', views.index),
-path('about',views.about),
+path('about/',views.about),
+path('contacts/',views.contacts),
 path('about/<user>',views.user),
 path('about/<user>/<int:age>',views.user),
 path('contacts',views.contact)
--- my_first/my_site/views.py
+++ my_first/my_site/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import models, form
 def index(request):
-    return HttpResponse("<h1>Главная</h1>")
+    return render(request, "base.html")
 def about(request):
-    return HttpResponse("<h2>О Нас</h2>")
+    if request.method == "POST":
+        name = request.POST.get("name")
+        date = request.POST.get("date")
+        print(name, date)
+        form1 = form.Form1(request.POST)
+        if form1.is_valid():
+            return HttpResponse("<h1>Форма валидна</h1>")
+    else:
+        form1 = form.Form1()
+    context = {
+        "zap": "text1",
+        "zap1":models.Model1.objects.all(),
+        "form": form1,
+    }
+    return render(request, "about.html",context)
 
 def contact(request):
     return HttpResponse("<h2>Контакты</h2>")
 
 def user(request, user, age ='29'):
     return HttpResponse(f"<h2>Имя: {user}, Возраст: {age}</h2>")
+def contacts (request):
+    return render(request, "contacts.html")
